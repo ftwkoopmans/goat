a6aebcbd0d683a9e788ffd21e0d43d677ab10ac3
Frank Koopmans <ftwkoopmans@gmail.com>
2024-12-07 18:50:49 +0100

    version 1.0.1

---
+++ _app/immutable/chunks/goat.25db362c.js
@@ -0,0 +1,1 @@
+function r(){return new Worker(""+new URL("../workers/goat-58b3cda8.js",import.meta.url).href)}export{r as default};
--- _app/immutable/chunks/goat.8c4bf35b.js
+++
@@ -1,1 +0,0 @@
-function r(){return new Worker(""+new URL("../workers/goat-4538ee89.js",import.meta.url).href)}export{r as default};
