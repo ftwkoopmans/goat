6eb79a18bab3e43412148bd73f4ada16b6617abf
Maurits Unkel <mauritsunkel@gmail.com>
2025-01-16 13:20:12 +0100

    Added score_type parameter and docs to test_genesets, as found in Github info

--- R/test_genesets.R
+++ R/test_genesets.R
@@ -39,6 +39,7 @@
 #' @param genesets tibble with genesets, must contain columns 'source', 'source_version', 'id', 'name', 'genes', 'ngenes', 'ngenes_signif'
 #' @param genelist tibble with genes, must contain column 'gene' and 'test'. gene = character column, which are matched against list column 'genes' in genesets tibble. test = boolean column (you can set all to FALSE if not performing Fisher-exact or hypergeometric test downstream)
 #' @param method method for overrepresentation analysis. Options: "goat", "hypergeometric", "fisherexact", "fisherexact_ease", "gsea", "idea"
+#' @param score_type string, default: "effectsize", alternatively set to "pvalue"
 #' @param padj_method first step of multiple testing correction; method for p-value adjustment, passed to `stats::p.adjust()` via `padjust_genesets()`, e.g. set "BH" to compute FDR adjusted p-values (default) or "bonferroni" for a more stringent procedure
 #' @param padj_sources second step of multiple testing correction; apply Bonferroni adjustment to all p-values according to the number of geneset sources that were tested. Boolean parameter, set TRUE to enable (default) or FALSE to disable
 #' @param padj_cutoff cutoff for adjusted p-value, `signif` column is set to TRUE for all values lesser-equals
@@ -47,7 +48,7 @@
 #' @param ... further parameters are passed to the respective stats method
 #' @return the input `genesets`, with results stored in columns 'pvalue', 'pvalue_adjust' and 'signif'
 #' @export
-test_genesets = function(genesets, genelist, method, padj_method = "BH", padj_sources = TRUE, padj_cutoff = 0.01, padj_min_signifgenes = 0L, ...) {
+test_genesets = function(genesets, genelist, method, score_type = "effectsize", padj_method = "BH", padj_sources = TRUE, padj_cutoff = 0.01, padj_min_signifgenes = 0L, ...) {
   algorithm = ngenes_signif = pvalue_adjust = pvalue = NULL # fix invisible bindings R package NOTE
   genesets = validate_genesets(genesets)
   genelist = validate_genelist(genelist)
